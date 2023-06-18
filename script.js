e910688697e3ca7ec20c61739a3fac11ef5f8951
Sadjoaldi <alsannealdi@outlook.com>
2023-06-18 05:15:01 +0200

    scroll popup

--- script.js
+++ script.js
@@ -7,3 +7,47 @@
 // Faire apparaitre la popup quand on est en bas du site
 
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
+
+const nav = document.querySelector("#navbar");
+const improvise = document.querySelector("#imgImprovise");
+const popup = document.querySelector(".pop-up");
+const popFerm = document.querySelector("#popup");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 120) {
+    nav.style.height = "50px";
+  } else {
+    nav.style.height = "100px";
+  }
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 280) {
+    improvise.style.transform = "translate(0)";
+    improvise.style.opacity = "1";
+  } else {
+    improvise.style.transform = "translate(-200px)";
+    improvise.style.opacity = "0";
+  }
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 1100) {
+    popup.style.transform = "translate(0)";
+    popup.style.opacity = "1";
+  } else {
+    popup.style.transform = "translateX(400px)";
+    popup.style.opacity = "0";
+  }
+});
+
+window.addEventListener("click", () => {
+  //   popFerm.style.display = "none";
+});
+// window.addEventListener('scroll', function() {
+//     var scrollPosition = window.scrollY;
+//     var pageHeight = document.documentElement.scrollHeight - window.innerHeight;
+//     if (scrollPosition === pageHeight) {
+//       // Afficher la popup
+//     }
+//   });
